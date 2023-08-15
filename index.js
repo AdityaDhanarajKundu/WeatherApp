ba546e4fdadf7fba07027ddf7443dde34c2854d1
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-08-15 21:49:32 +0530

    added the function to the button click fetches the city from the input text field

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const cityEl = document.querySelector(".city");
 const tempEl = document.querySelector(".temp");
 const humidEl = document.querySelector(".humidity");
 const windEl = document.querySelector(".wind");
+const inputEl = document.getElementById("input-city");
+const buttonEl = document.querySelector(".search button");
 
 const apiKey = "b60fb16966d8bbb8d84562d84c6706d2";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=Bengaluru&appid=${apiKey}`;
-
 async function checkWeather(){
-    let city = 
+    let city = inputEl.value;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+
     try{
         let data = await fetch(apiUrl).then(response=>{return response.json();});
         cityEl.textContent = data.name;
@@ -21,4 +23,4 @@ async function checkWeather(){
     }
 }
 
-checkWeather();
+buttonEl.addEventListener("click",checkWeather);
